ba67815baf64f781bedcb165cd31ab425b014b19
Michal Balogh <baloghmichal03@gmail.com>
2024-04-20 23:01:38 +0200

    test: more advanced test cases

--- src/NetworkSniffer.Tests/tests.py
+++ src/NetworkSniffer.Tests/tests.py
@@ -22,9 +22,9 @@ import sys
 import unittest
 import re
 import subprocess
-import os
 import time
 from scapy.all import *
+from scapy.contrib.igmp import *
 from termcolor import colored
 import threading
 
@@ -90,44 +90,232 @@ class TestIPKSniffer(unittest.TestCase):
 
         return test_result
 
+    def check_header_ipv4(self, output, frame_length):
+        self.assertIn(f"frame length: {frame_length} bytes", output, colored(f"Frame length {frame_length} bytes not found in output.", "red"))
+        self.assertIn("src MAC: 00:00:00:00:00:00", output, colored("Src MAC 00:00:00:00:00:00 not found in output.", "red"))
+        self.assertIn("dst MAC: ff:ff:ff:ff:ff:ff", output, colored("Dst MAC ff:ff:ff:ff:ff:ff not found in output.", "red"))
+        self.assertIn("src IP: 127.0.0.1", output, colored("Src IP 127.0.0.1 not found in output.", "red"))
+        self.assertIn("dst IP: 127.0.0.1", output, colored("Dst IP 127.0.0.1 not found in output.", "red"))
+
+    def check_header_ipv6(self, output, frame_length):
+        self.assertIn(f"frame length: {frame_length} bytes", output, colored(f"Frame length {frame_length} bytes not found in output.", "red"))
+        self.assertIn("src MAC: 00:00:00:00:00:00", output, colored("Src MAC 00:00:00:00:00:00 not found in output.", "red"))
+        self.assertIn("dst MAC: ff:ff:ff:ff:ff:ff", output, colored("Dst MAC ff:ff:ff:ff:ff:ff not found in output.", "red"))
+        self.assertIn("src IP: ::1", output, colored("Src IP ::1 not found in output.", "red"))
+        self.assertIn("dst IP: ::1", output, colored("Dst IP ::1 not found in output.", "red"))
+
     def send_tcp(self, data):
-        packet = IP(dst="127.0.0.1") / TCP(dport=80) / data
+        packet = IP(dst="127.0.0.1") / TCP(dport=80, sport=20) / data
         send(packet)
         time.sleep(0.1)
         return self.sniffer_process.stdout.read().strip()
 
     def send_udp(self, data):
-        packet = IP(dst="127.0.0.1") / UDP(dport=53) / data
+        packet = IP(dst="127.0.0.1") / UDP(dport=53, sport=20) / data
+        send(packet)
+        time.sleep(0.1)
+        return self.sniffer_process.stdout.read().strip()
+
+    def send_icmp4(self, data):
+        packet = IP(dst="127.0.0.1") / ICMP() / data
+        send(packet)
+        time.sleep(0.1)
+        return self.sniffer_process.stdout.read().strip()
+
+    def send_icmp6(self, data):
+        packet = IPv6(dst="::1") / ICMPv6EchoRequest() / data
+        send(packet)
+        time.sleep(0.1)
+        return self.sniffer_process.stdout.read().strip()
+
+    def send_arp(self, data):
+        packet = ARP(pdst="127.0.0.1") / data
+        send(packet)
+        time.sleep(0.1)
+        return self.sniffer_process.stdout.read().strip()
+
+    def send_ndp(self, data):
+        packet = IPv6(dst="::1") / ICMPv6ND_NS() / data
+        send(packet)
+        time.sleep(0.1)
+        return self.sniffer_process.stdout.read().strip()
+
+    def send_igmp(self, data):
+        packet = IP(dst="127.0.0.1") / IGMP() / data
+        send(packet)
+        time.sleep(0.1)
+        return self.sniffer_process.stdout.read().strip()
+
+    def send_mld(self, data):
+        packet = IPv6(src="::1", dst="::1") / ICMPv6MLQuery() / data
         send(packet)
         time.sleep(0.1)
         return self.sniffer_process.stdout.read().strip()
 
     def test_tcp(self):
-        args = ["-i", "lo", "--port-destination", "80"]
+        args = ["-i", "lo", "--tcp", "--port-destination", "80"]
         data = "Hello,World!"
         output = self.run_test_with_timeout(self.send_tcp, args, data)
-        self.assertIn("dst port: 80", output, colored("Port 80 not found in output.", "red"))
+        self.assertIn("src port: 20", output, colored("Src port 20 not found in output.", "red"))
+        self.assertIn("dst port: 80", output, colored("Dst port 80 not found in output.", "red"))
+        self.check_header_ipv4(output, 66)
         self.assertInPayload(data, output)
         print(colored("Test passed.", "green"))
 
     def test_udp(self):
-        args = ["-i", "lo", "--port-destination", "53"]
+        args = ["-i", "lo", "--udp", "--port-destination", "53"]
         data = "Hello,World!"
         output = self.run_test_with_timeout(self.send_udp, args, data)
-        self.assertIn("dst port: 53", output, colored("Port 53 not found in output.", "red"))
+        self.assertIn("dst port: 53", output, colored("dst port 53 not found in output.", "red"))
+        self.assertIn("src port: 20", output, colored("src port 20 not found in output.", "red"))
+        self.check_header_ipv4(output, 54)
         self.assertInPayload(data, output)
         print(colored("Test passed.", "green"))
 
+    def test_icmp4(self):
+        args = ["-i", "lo", "--icmp4"]
+        data = "DATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATADATA"
+        output = self.run_test_with_timeout(self.send_icmp4, args, data)
+        self.check_header_ipv4(output, 210)
+        self.assertInPayload(data, output)
+        print(colored("Test passed.", "green"))
+
+    def test_icmp6(self):
+        args = ["-i", "lo", "--icmp6"]
+        data = "ICMP6heyho"
+        output = self.run_test_with_timeout(self.send_icmp6, args, data)
+        self.check_header_ipv6(output, 72)
+        self.assertInPayload("", output)
+        print(colored("Test passed.", "green"))
+
+    def test_arp(self):
+        args = ["-i", "lo", "--arp"]
+        data = "Hello,World!"
+        output = self.run_test_with_timeout(self.send_arp, args, data)
+        self.check_header_ipv4(output, 42)
+        print(colored("Test passed.", "green"))
+
+    def test_ndp(self):
+        args = ["-i", "lo", "--ndp"]
+        data = "NDPNDPNDPNDPNDPNDPNDPNDPNDPNDPNDPNDP"
+        output = self.run_test_with_timeout(self.send_ndp, args, data)
+        self.check_header_ipv6(output, 118)
+        self.assertInPayload(data, output)
+        print(colored("Test passed.", "green"))
+
+    def test_igmp(self):
+        args = ["-i", "lo", "--igmp"]
+        data = "igmp"
+        output = self.run_test_with_timeout(self.send_igmp, args, data)
+        self.check_header_ipv4(output, 46)
+        self.assertInPayload(data, output)
+        print(colored("Test passed.", "green"))
+
+    def test_mld(self):
+        args = ["-i", "lo", "--mld"]
+        data = "MLD_MLD_MLD_MLD"
+        output = self.run_test_with_timeout(self.send_mld, args, data)
+        self.check_header_ipv6(output, 93)
+        self.assertInPayload(data, output)
+        print(colored("Test passed.", "green"))
+
+    def test_mulitple_packets(self):
+        args = ["-i", "lo", "-n", "3", "--ndp", "--mld", "--igmp"]
+        data = "FirstPacket"
+        data2 = "SecondPacket"
+        data3 = "ThirdPacket"
+        output = self.run_test_with_timeout(self.send_ndp, args, data)
+        output2 = self.run_test_with_timeout(self.send_mld, args, data2)
+        output3 = self.run_test_with_timeout(self.send_igmp, args, data3)
+        self.assertInPayload(data, output)
+        self.assertInPayload(data2, output2)
+        self.assertInPayload(data3, output3)
+        print(colored("Test passed.", "green"))
+
+    def test_tcp_or_udp(self):
+        args = ["-i", "lo", "--tcp", "--udp", "--port-source", "20", "-n", "2"]
+        data = "FirstPacketTCP"
+        data2 = "SecondPacketUDP"
+        output = self.run_test_with_timeout(self.send_tcp, args, data)
+        output2 = self.run_test_with_timeout(self.send_udp, args, data2)
+        self.assertIn("dst port: 80", output, colored("Port 80 not found in output.", "red"))
+        self.check_header_ipv4(output, 68)
+        self.assertInPayload(data, output)
+        self.check_header_ipv4(output2, 57)
+        self.assertInPayload(data2, output2)
+        print(colored("Test passed.", "green"))
+
     def test_filter1(self):
-        args = ["-i", "lo", "--tcp", "--port-destination", "80"]
+        args = ["-i", "lo", "--tcp", "-p", "20"]
+        data1 = "Hello,World!"
+        data2 = "BarFoo"
+        self.run_test_with_timeout(self.send_udp, args, data1)
+        output = self.run_test_with_timeout(self.send_tcp, args, data2)
+        self.assertIn("src port: 20", output, colored("Port 20 not found in output.", "red"))
+        self.assertIn("dst port: 80", output, colored("Port 80 not found in output.", "red"))
+        self.assertInPayload(data2, output)
+        print(colored("Test passed.", "green"))
+
+    def test_filter2(self):
+        args = ["-i", "lo", "--udp", "-p", "20"]
+        data1 = "Hello,World!"
+        data2 = "BarFoo"
+        self.run_test_with_timeout(self.send_tcp, args, data1)
+        output = self.run_test_with_timeout(self.send_udp, args, data2)
+        self.assertIn("src port: 20", output, colored("Port 20 not found in output.", "red"))
+        self.assertIn("dst port: 53", output, colored("Port 53 not found in output.", "red"))
+        self.assertInPayload(data2, output)
+        print(colored("Test passed.", "green"))
+
+    def test_filter3(self):
+        args = ["-i", "lo", "--icmp4", "--icmp6"]
+        data1 = "Hello,World!"
+        data2 = "BarFoo"
+        self.run_test_with_timeout(self.send_udp, args, data1)
+        output = self.run_test_with_timeout(self.send_icmp4, args, data2)
+        self.check_header_ipv4(output, 48)
+        self.assertInPayload(data2, output)
+        print(colored("Test passed.", "green"))
+
+    def test_filter4(self):
+        args = ["-i", "lo", "--arp", "--ndp"]
+        data1 = "Hello,World!"
+        data2 = "BarFoo"
+        self.run_test_with_timeout(self.send_udp, args, data1)
+        output = self.run_test_with_timeout(self.send_arp, args, data2)
+        self.check_header_ipv4(output, 42)
+        print(colored("Test passed.", "green"))
+
+    def test_filter5(self):
+        args = ["-i", "lo", "--igmp", "--mld"]
+        data1 = "Hello,World!"
+        data2 = "BarFoo"
+        self.run_test_with_timeout(self.send_udp, args, data1)
+        output = self.run_test_with_timeout(self.send_igmp, args, data2)
+        self.check_header_ipv4(output, 48)
+        self.assertInPayload(data2, output)
+        print(colored("Test passed.", "green"))
+
+    def test_filter6(self):
+        args = ["-i", "lo", "--tcp", "--udp", "--port-destination", "80"]
         data1 = "Hello,World!"
         data2 = "BarFoo"
         self.run_test_with_timeout(self.send_udp, args, data1)
         output = self.run_test_with_timeout(self.send_tcp, args, data2)
         self.assertIn("dst port: 80", output, colored("Port 80 not found in output.", "red"))
+        self.check_header_ipv4(output, 60)
         self.assertInPayload(data2, output)
         print(colored("Test passed.", "green"))
 
+    def test_filter7(self):
+        args = ["-i", "lo", "--icmp4", "--icmp6", "--port-source", "20"]
+        data1 = "Hello,World!"
+        data2 = "BarFoo"
+        self.run_test_with_timeout(self.send_udp, args, data1)
+        output = self.run_test_with_timeout(self.send_icmp4, args, data2)
+        self.check_header_ipv4(output, 48)
+        self.assertInPayload(data2, output)
+        print(colored("Test passed.", "green"))
 
     def test_no_package_with_set_filter(self):
         args = ["-i", "lo", "--port-destination", "54"]
@@ -135,6 +323,23 @@ class TestIPKSniffer(unittest.TestCase):
         output = self.run_test_with_timeout(self.send_udp, args, data)
         self.assertIn("", output, colored("No packets should be sniffed", "red"))
         print(colored("Test passed.", "green"))
+
+    def test_catch_ndp_with_icmp6_fiter(self):
+        args = ["-i", "lo", "--icmp6"]
+        data = "Hello,World!"
+        output = self.run_test_with_timeout(self.send_ndp, args, data)
+        self.check_header_ipv6(output, 94)
+        self.assertInPayload(data, output)
+        print(colored("Test passed.", "green"))
+
+    def test_catch_mld_with_icmp6_fiter(self):
+        args = ["-i", "lo", "--icmp6"]
+        data = "Hello,World!"
+        output = self.run_test_with_timeout(self.send_mld, args, data)
+        self.check_header_ipv6(output, 90)
+        self.assertInPayload(data, output)
+        print(colored("Test passed.", "green"))
+
 
 
 
